7e3dbe1b4f1ac96450acdcb2a60c58a26ef04456
anderrf <ander_rochafarias@live.com>
2020-12-22 22:49:08 -0300

    adicao de marcador no mapa da criacao de problema

--- scripts/page-createComplaint.js
+++ scripts/page-createComplaint.js
@@ -9,3 +9,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }*/)
     .addTo(map);
+
+//create icon
+
+const icon = L.icon({
+        iconUrl: "../images/marker.svg",
+        iconSize: [56, 68],
+        iconAnchor: [29, 68],
+        popupAnchor: [170, 2]
+});
+
+let marker;
+
+//create and add marker
+map.on('click', (event)=>{
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+    document.getElementById('latLng').dataset.lat = lat;
+    document.getElementById('latLng').dataset.lng = lng;
+    //remove icon
+    marker && map.removeLayer(marker);
+    //add icon layer
+    marker = L.marker([lat, lng], {icon})
+        .addTo(map);
+});
