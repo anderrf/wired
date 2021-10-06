cd93cddff8cf5973157687495be116165f6cf9e4
anderrf <anderson_rocha_farias@outlook.com>
2021-10-06 14:44:08 -0300

    Requisição de logon

--- src/server.js
+++ src/server.js
@@ -10,7 +10,20 @@ const server = express();
 
 //Configurar session
 const time = (1000*60*60*24);
+server.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: { maxAge: time },
+    resave: false
+}));
 
+server
+    .use(express.json())
+    .use(express.static(__dirname))
+    .use(cookieParser());
+var session;
+const fakeEmail = "teste@teste.com";
+const fakePassword = "testando";
 
 //Rotas
 server
@@ -28,7 +41,18 @@ server
     .get('/cadastre-se', pages.signup)
     .get('/problema', pages.complaint)
     .get('/criar-problema', pages.createComplaint)
-    .post('/cadastrar-usuario', pages.createUser);
+    .post('/cadastrar-usuario', pages.createUser)
+    .post('/usuario-entrar', (req, res) => {
+        if(req.body.logEmail == fakeEmail && req.body.logPassword == fakePassword){
+            session = req.session;
+            session.userId = req.body.logEmail;
+            session.userName = req.body.logEmail;
+            res.redirect('/mapa');
+        }
+        else{
+            res.send("Usuário ou senha inválidos!");
+        }
+    });
 
 //Determina a porta de execução do servidor
 server.listen(5500);
